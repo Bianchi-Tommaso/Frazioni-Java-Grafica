410691fa92409e53db0fa39f5e07d79180861263
tommy bianchi <tbianchi866@gmail.com>
2021-04-16 14:52:39 +0200

    Frazioni Java Grafica

--- src/main/java/Frazioni.java
+++ src/main/java/Frazioni.java
@@ -3,57 +3,6 @@ public class Frazioni
 {
     public static void main(String[] args) 
     {
-        GraficaFrazioni gf = new GraficaFrazioni();
-        /*
-        Razionale uno     = new Razionale(1,5); //somm
-        Razionale due     = new Razionale(2,7); //somm
-        // Razionale tre  = new Razionale(3,0);
-        Razionale quattro = new Razionale();    //ris
-        Razionale cinque  = new Razionale(4,20);//riduci e sottrazione 
-        Razionale sei     = new Razionale(6,9); //sottrazione
-        Razionale sette   = new Razionale(12,9);//moltiplicazione
-        Razionale otto    = new Razionale(23,43);//moltiplicazione
-        Razionale nove    = new Razionale(27,76);//divisione
-        Razionale dieci   = new Razionale(4,5);  //divisione
-        Razionale undici  = new Razionale(1,3);  //reciproco
-        Razionale dodici  = new Razionale(2,4);  //isZero
-        Razionale tredici = new Razionale(-2,2);  //isPositive confronto
-        Razionale quattordici = new Razionale(-2,2);  //isPositive confronto
-        
-        //Test Somma.  
-        quattro = uno.somma(due);
-        System.out.println("La Somma e: " + quattro.getN() + " / " + quattro.getD());
-        
-        //Test Riduci.
-        System.out.println("La Frazione e: " + cinque.getN() + " / " + cinque.getD());
-        cinque.riduci();
-        System.out.println("La Riduzione e: " + cinque.getN() + " / " + cinque.getD());
-        
-        //Test Sottrazione.
-        quattro = cinque.sottrazione(sei);
-        System.out.println("La Sottrazione e: " + quattro.getN() + " / " + quattro.getD());
-         
-        //Test Moltiplicazione.
-        quattro = sette.prodotto(otto);
-        System.out.println("Il Prodotto e: " + quattro.getN() + " / " + quattro.getD());
-        
-        //Test Divisione.
-        quattro = nove.quoziente(dieci);
-        System.out.println("La Divisione e: " + quattro.getN() + " / " + quattro.getD());
-        
-        //Test Reciprico.
-        quattro = undici.reciproco();
-        System.out.println("Il reciproco e: " + quattro.getN() + " / " + quattro.getD());
-        
-        //Test isZero.
-         System.out.println(dodici.isZero());
-         
-         //Test isPositive.
-         System.out.println(tredici.isPositive());
-         
-         //Test Confronto.
-         System.out.println(tredici.confronto(quattordici));
-         */
-    }
-    
+        GraficaFrazioni gf = new GraficaFrazioni(); //Chiamo dal Main il costruttore per poter lanciare il programma.
+    }   
 }
--- src/main/java/GraficaFrazioni.java
+++ src/main/java/GraficaFrazioni.java
@@ -1,45 +1,46 @@
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-class GraficaFrazioni extends JFrame implements ActionListener
+class GraficaFrazioni extends JFrame implements ActionListener  
 {
-        private Container c = new Container();
-        private JPanel p1 = new JPanel();
-        private JLabel numeratore = new JLabel ("Inserire Numeratore:");
-        private JLabel denominatore = new JLabel("Inserire Denominatore:");
-        private JLabel risultato = new JLabel("");
-        private JTextField N = new JTextField();
+        private Container c = new Container();    //Container, conterrà tutta l'interfaccia.
+        private JPanel p1 = new JPanel();         //JPanel, conterrà tutti i JButton, JTextField e JLabel.
+        private JLabel numeratore = new JLabel ("Inserire Numeratore:");     //JLabel con scritto "Inserire Numeratore".
+        private JLabel denominatore = new JLabel("Inserire Denominatore:");  //JLabel con scritto "Inserire Denominatore". 
+        private JLabel risultato = new JLabel("");                           //JLabel vuoto che ha la funzione di stampare il risultato delle Frazioni.
+        private JTextField N = new JTextField();                             //JTextField serve come input per inserire il Numeratore o Denominatore
         private JTextField D = new JTextField();
         private JTextField N1 = new JTextField();
         private JTextField D1 = new JTextField();
-        private JButton bottoneReciproco = new JButton("Reciproco");
-        private JButton bottoneSomma = new JButton("Somma");
-        private JButton bottoneSottrazione = new JButton("Sottrazione");
-        private JButton bottoneProdotto = new JButton("Prodotto");
-        private JButton bottoneQuoziente = new JButton("Quoziente");
+        private JButton bottoneReciproco = new JButton("Reciproco");         //JButton ha la funzione di generare un evento, in questo caso l'evento del Reciproco.
+        private JButton bottoneSomma = new JButton("Somma");                 //JButton genera l'evento della somma
+        private JButton bottoneSottrazione = new JButton("Sottrazione");     //JButton genera l'evento della sottrazione
+        private JButton bottoneProdotto = new JButton("Prodotto");           //JButton genera l'evento del prodotto
+        private JButton bottoneQuoziente = new JButton("Quoziente");         //JButton genera l'evento del quoziente.
         
-    public GraficaFrazioni() 
+    public GraficaFrazioni()          //Questo è il costruttore dove impostiamo tutti i parametri necessari per creare la nostra grafica
     {
-        c = this.getContentPane();
-        p1.setLayout(null);
-        this.setTitle("Frazioni");
-        this.setVisible(true);
-        this.setLocationRelativeTo(null);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.pack();
-        this.setSize(250, 300);
-        this.setResizable(false); 
-        p1.setBackground(Color.orange);
+        c = this.getContentPane();      
+        p1.setLayout(null);          //Il layout viene impostato a NULL, in modo da poter gestire liberamente dove collocare ogni componente.
+        this.setTitle("Frazioni");                              //Imposta il titolo alla finestra.
+        this.setVisible(true);                                  //Imposta la finestra visibile.
+        this.setLocationRelativeTo(null);                       //Imposta la finestra al centro dello schermo
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    //Imposta la chiusura della finestra
+        this.setSize(250, 300);                                 //Imposta la grandezza del Frame(la finestra)
+        this.setResizable(false);                               //Imposta in modo che la finestra non si possa ridimensionare
+        p1.setBackground(Color.orange);                         //Imposta lo sfondo del JPanel.
+        
+        
+        //Qui vengono inserite tutte le cordinate e dimensioni di ogni singolo elemento. setBounds(x, y, lunghezza, larghezza)
         
         numeratore.setBounds(60, 5, 140, 50);
         denominatore.setBounds(60, 65, 140, 50);
@@ -53,6 +54,18 @@ class GraficaFrazioni extends JFrame implements ActionListener
         bottoneSottrazione.setBounds(20, 180, 90, 20);
         bottoneProdotto.setBounds(130, 180, 90, 20);
         bottoneQuoziente.setBounds(75, 205, 90, 20);
+        
+        
+        //Qui viene impostato il font ad ogni singolo bottone
+        
+        bottoneReciproco.setFont(new Font("Arial", Font.PLAIN, 11));
+        bottoneSomma.setFont(new Font("Arial", Font.PLAIN, 11));
+        bottoneSottrazione.setFont(new Font("Arial", Font.PLAIN, 11));
+        bottoneProdotto.setFont(new Font("Arial", Font.PLAIN, 11));   
+        bottoneQuoziente.setFont(new Font("Arial", Font.PLAIN, 11));
+        bottoneReciproco.setFont(new Font("Arial", Font.PLAIN, 11));
+              
+        //Viene aggiunto al JPanel ogni singolo componente e infine il JPanle viene aggiunto al Container
         
         p1.add(numeratore);
         p1.add(denominatore);
@@ -68,6 +81,8 @@ class GraficaFrazioni extends JFrame implements ActionListener
         p1.add(bottoneQuoziente);
         c.add(p1);
         
+        //Qui viene impostato l'ascoltatore dell'evento che viene generato.
+        
         bottoneReciproco.addActionListener(this);
         bottoneSomma.addActionListener(this);
         bottoneSottrazione.addActionListener(this);
@@ -76,34 +91,38 @@ class GraficaFrazioni extends JFrame implements ActionListener
     }
 
     @Override
-    public void actionPerformed(ActionEvent e)
+    public void actionPerformed(ActionEvent e) //Questo metodo ci permette di gestire gli eventi generati.
     {
-        String evento = e.getActionCommand();
-        String letturaNumeratore;
+        String evento = e.getActionCommand();   //Questa Stringa cattura l'evento generato, così facendo possiamo gestirlo
+        String letturaNumeratore;               //Legge il JTextField del Numeratore, ovvero il dato che viene inserito dall'utente.
         String letturaDenominatore;
         String letturaNumeratore1;
         String letturaDenominatore1;
-        String outputRisultato = null;
-        Razionale stampa;
+        String outputRisultato = null;          
+        Razionale stampa;                       //Oggetto per poter stampare il risultato
 
-        switch(evento)
+        switch(evento)                          //Switch per gestire l'evento e capire quale evento è stato generato
         {
-            case "Reciproco":
-                letturaNumeratore = N.getText();
-                letturaDenominatore = D.getText();
-                letturaNumeratore1 = N1.getText();
-                letturaDenominatore1 = D1.getText();
-                Razionale frazione = new Razionale(Integer.valueOf(letturaNumeratore), Integer.valueOf(letturaDenominatore));
+            case "Reciproco":                   
+                letturaNumeratore = N.getText();      //Legge il Numeratore nel JTextField di N
+                letturaDenominatore = D.getText();    //Legge il Denominatore nel JTextField di D
+                letturaNumeratore1 = N1.getText();     
+                letturaDenominatore1 = D1.getText(); 
+                
+                //Instanza dell'oggetto per poter inserire i dati e calcolarli. Viene usato Integer.valueOf per poter convertire una stringa in un numero
+                
+                Razionale frazione = new Razionale(Integer.valueOf(letturaNumeratore), Integer.valueOf(letturaDenominatore));   
                 Razionale frazione1 = new Razionale(Integer.valueOf(letturaNumeratore1), Integer.valueOf(letturaDenominatore1));
-                stampa = frazione.reciproco();
-                outputRisultato = stampa.toString();
-                stampa = frazione1.reciproco();
-                outputRisultato += "|";
-                outputRisultato += stampa.toString();
-                risultato.setText(outputRisultato);
+                
+                stampa = frazione.reciproco();              //Chiamo il metodo reciproco per la prima frazione
+                outputRisultato = stampa.toString();        //Lo salvo in outputRisultato
+                stampa = frazione1.reciproco();             //Chiamo il metodo reciproco per la seconda frazione
+                outputRisultato += " | ";                   //Concateno al risultato " | " per poter dividere le due frazioni.
+                outputRisultato += stampa.toString();       //concateno il risultato della seconda frazione
+                risultato.setText(outputRisultato);         //Infine stampo il risultato nel JLabel risultato
                 break;
                 
-            case "Somma":
+            case "Somma":                       //Il Procedimento è lo stesso per tutti gli altri casi, cambia soltanto il richiamo di alcuni metodi.
                 letturaNumeratore = N.getText();
                 letturaDenominatore = D.getText();
                 letturaNumeratore1 = N1.getText();
