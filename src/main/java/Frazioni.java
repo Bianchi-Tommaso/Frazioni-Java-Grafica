ebf872a672e1a9c5939d05087da644eed1a0cc10
Bianchi-Tommaso <tbianchi866@gmail.com>
2021-11-07 00:47:29 +0100

    test

--- src/main/java/Frazioni.java
+++
@@ -1,8 +0,0 @@
-
-public class Frazioni
-{
-    public static void main(String[] args) 
-    {
-        GraficaFrazioni gf = new GraficaFrazioni(); //Chiamo dal Main il costruttore per poter lanciare il programma.
-    }   
-}
---
+++ test.java
@@ -0,0 +1,14 @@
+public class test
+{
+    public static void main(String [] args) 
+    {
+        System.out.println("Ciao Mondo");
+
+        Moltiplica(4, 86);
+    }
+
+    public static void Moltiplica(int x, int y)
+    {
+        System.out.println("Moltiplicazione: " + x * y);
+    }
+}
